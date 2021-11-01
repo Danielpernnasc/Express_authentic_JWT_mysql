c3b99f521e019bb5884a8f06c31491ac1c1cc200
danielpernasc <dpericles6@gmail.com>
2021-11-01 17:00:58 -0300

    primeiro commit

---
+++ app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "Dan81#Cris82",
+    DB: "dbmysql",
+    dialect: "mysql",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
